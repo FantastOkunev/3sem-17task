e297463091e928d596f018044d2677c6443ce99f
FantastOkunev <okunev229@yandex.ru>
2021-03-12 14:35:33 +0300

    version 3

--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 a.exe:test.o
-	wg++ -g -fno-elide-constructors -std=c++11 test.o
-test.o:test.cpp
-	wg++ -g -fno-elide-constructors -std=c++11 test.cpp   -c
+	g++ -W -Wall -Wfloat-equal -Wpointer-arith -Wwrite-strings -Wcast-align -Wformat-security -Wmissing-format-attribute -Wformat=1 -Wno-long-long -Wcast-align -Winline -Werror -pedantic -pedantic-errors -Wunused -Wuninitialized --param inline-unit-growth=1000000 --param max-inline-insns-single=10000000 --param large-function-growth=10000000 -g test.o
+test.o:test.cpp header.h
+	g++ -W -Wall -Wfloat-equal -Wpointer-arith -Wwrite-strings -Wcast-align -Wformat-security -Wmissing-format-attribute -Wformat=1 -Wno-long-long -Wcast-align -Winline -Werror -pedantic -pedantic-errors -Wunused -Wuninitialized --param inline-unit-growth=1000000 --param max-inline-insns-single=10000000 --param large-function-growth=10000000 -g test.cpp   -c
+
--- test.cpp
+++ test.cpp
@@ -11,7 +11,19 @@ class CPoly2
 protected:
 	int **arr = nullptr;
 
-	int N;
+	int N = 0;
+
+private:
+	void delete_arr()
+	{
+		for (int i = 0; i < N; i++)
+		{
+			delete arr[i];
+			arr[i] = nullptr;
+		}
+		delete arr;
+		arr = nullptr;
+	}
 
 public:
 	CPoly2(int **arr_, int N_)
@@ -19,14 +31,121 @@ public:
 		arr = arr_;
 		N = N_;
 	}
-	~CPoly2()
+	virtual ~CPoly2()
+	{
+		delete_arr();
+	}
+	/*
+	CPoly2(const CPoly2 &other)
+	{
+		N = other.N;
+		arr = new int *[N];
+		for (int i = 0; i < N; i++)
+		{
+			arr[i] = new int[N];
+			for (int j = 0; j < N; j++)
+			{
+				arr[i][j] = other.arr[i][j];
+			}
+		}
+	}
+	CPoly2(CPoly2 &&other)
+	{
+		N = other.N;
+		other.N = 0;
+		arr = other.arr;
+		other.arr = nullptr;
+	}
+	CPoly2 &operator=(const CPoly2 &other)
+	{
+		if (this == &other)
+		{
+			cout << "a = a" << endl;
+			return *this;
+		}
+		delete_arr();
+		N = other.N;
+		arr = new int *[N];
+		for (int i = 0; i < N; i++)
+		{
+			arr[i] = new int[N];
+			for (int j = 0; j < N; j++)
+			{
+				arr[i][j] = other.arr[i][j];
+			}
+		}
+		return *this;
+	}
+	CPoly2 &operator=(CPoly2 &&other)
+	{
+		delete_arr();
+		N = other.N;
+		arr = other.arr;
+		return *this;
+	}
+	CPoly2 operator+(const CPoly2 &other)
+	{
+		int sum_N = max(N, other.N);
+		int **sum_arr = new int *[sum_N];
+		arr = new int *[N];
+		for (int i = 0; i < N; i++)
+		{
+			arr[i] = new int[N];
+			for (int j = 0; j < N; j++)
+			{
+				arr[i][j] = 0;
+				if (i <= other.N and j <= other.N)
+					arr[i][j] += other.arr[i][j];
+				if (i <= other.N and j <= N)
+					arr[i][j] += arr[i][j];
+			}
+		}
+		return CPoly2(sum_arr, sum_N);
+	}
+	CPoly2 operator-(const CPoly2 &other)
+	{
+		int sum_N = max(N, other.N);
+		int **sum_arr = new int *[sum_N];
+		arr = new int *[N];
+		for (int i = 0; i < N; i++)
+		{
+			arr[i] = new int[N];
+			for (int j = 0; j < N; j++)
+			{
+				arr[i][j] = 0;
+				if (i <= other.N and j <= other.N)
+					arr[i][j] -= other.arr[i][j];
+				if (i <= other.N and j <= N)
+					arr[i][j] += arr[i][j];
+			}
+		}
+		return CPoly2(sum_arr, sum_N);
+	}
+	CPoly2 operator--(void)
 	{
 		for (int i = 0; i < N; i++)
 		{
-			delete arr[i];
+			for (int j = 0; j < N - 1; j++)
+			{
+				arr[i][j] = arr[i][j + 1];
+			}
+			arr[i][N - 1] = 0;
 		}
-		delete arr;
+		return *this;
 	}
+	CPoly2 operator++(void)
+	{
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N - 1; j++)
+			{
+				arr[j][i] = arr[j + 1][i];
+			}
+			arr[N - 1][i] = 0;
+		}
+		return *this;
+	}
+	*/
 	virtual int output() = 0;
 	static CPoly2 *CreateData(char *, CFabricData **);
 };
@@ -118,10 +237,17 @@ class CFabricData1 : public CFabricData
 CPoly2 *CPoly2::CreateData(char *str, CFabricData **f)
 {
 	CPoly2 *tmp = nullptr;
-	int N = 2;
+	int N = 2, I = 1;
 	int **arr = new int *[N];
-	char *pch = strtok(str, " .,"), *fname = nullptr;
-	fname = strtok(nullptr, " .,");
+	char *pch = strtok(str, " "), *fname = nullptr;
+	if (pch[0] == '0')
+		I = 0;
+	pch = strtok(nullptr, " ");
+	N *= 1;
+	fname = new char[strlen(pch) + 1];
+
+	strcpy(fname, pch);
+	cout << fname << " ";
 
 	for (int i = 0; i < N; i++)
 	{
@@ -131,17 +257,17 @@ CPoly2 *CPoly2::CreateData(char *str, CFabricData **f)
 			arr[i][j] = i * N + j;
 		}
 	}
-	if (str[0] == '0')
-		tmp = f[0]->create("output.txt", arr, N);
+	if (I == 0)
+		tmp = f[0]->create(fname, arr, N);
 	else
-		tmp = f[1]->create("output.txt", arr, N);
+		tmp = f[1]->create(fname, arr, N);
 	return tmp;
 }
 
 int main()
 {
 
-	int I, len_arr = 0, size_arr = 4;
+	int len_arr = 0, size_arr = 4;
 	char str[32];
 
 	ifstream fin("input.txt");
