947026c1e2a949e03b6e6969d17db406701e87cd
FantastOkunev <okunev229@yandex.ru>
2021-03-30 20:05:00 +0300

    don't work

--- test.cpp
+++ test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include <cmath>
 #include <vector>
 #include <string>
 
@@ -281,183 +280,194 @@ CPoly2 *CPoly2::CreateData(char str[], CFabricData **f)
 	return tmp;
 }
 
-// CData0 operator+(const CPoly2 &first, const CPoly2 &second)
-// {
-// 	int N_sum = max(first.N, second.N);
-// 	int **arr_sum = new int *[N_sum];
-// 	char *fname_sum = new char[strlen(first.cfname) + 1];
-// 	strcpy(fname_sum, first.cfname);
-// 	for (int i = 0; i < N_sum; i++)
-// 	{
-// 		arr_sum[i] = new int[N_sum];
-// 		for (int j = 0; j < N_sum; j++)
-// 		{
-// 			arr_sum[i][j] = 0;
-// 			if (i < first.N and j < first.N)
-// 				arr_sum[i][j] += first.arr[i][j];
-// 			if (i < second.N and j < second.N)
-// 				arr_sum[i][j] += second.arr[i][j];
-// 		}
-// 	}
-// 	CData0 val(nullptr, nullptr, 0);
-// 	CData0 sum(fname_sum, arr_sum, N_sum);
-// 	cout << "end sum\n";
-// 	return sum;
-// }
+CData0 operator+(const CPoly2 &first, const CPoly2 &second)
+{
+	int N_sum = max(first.N, second.N);
+	vector<vector<int>> arr_sum;
+	string fname_sum = first.cfname;
+	arr_sum.resize(N_sum);
+	for (int i = 0; i < N_sum; i++)
+	{
+		arr_sum[i].resize(N_sum);
+		for (int j = 0; j < N_sum; j++)
+		{
+			arr_sum[i][j] = 0;
+			if (i < first.N and j < first.N)
+				arr_sum[i][j] += first.arr[i][j];
+			if (i < second.N and j < second.N)
+				arr_sum[i][j] += second.arr[i][j];
+		}
+	}
+	CData0 sum(fname_sum, arr_sum, N_sum);
+	// cout << "end sum\n";
+	return sum;
+}
 
-// CData0 operator-(const CPoly2 &first, const CPoly2 &second)
-// {
-// 	int N_sum = max(first.N, second.N);
-// 	int **arr_sum = new int *[N_sum];
-// 	char *fname_sum = new char[strlen(first.cfname) + 1];
-// 	strcpy(fname_sum, first.cfname);
-// 	for (int i = 0; i < N_sum; i++)
-// 	{
-// 		arr_sum[i] = new int[N_sum];
-// 		for (int j = 0; j < N_sum; j++)
-// 		{
-// 			arr_sum[i][j] = 0;
-// 			if (i < first.N and j < first.N)
-// 				arr_sum[i][j] += first.arr[i][j];
-// 			if (i < second.N and j < second.N)
-// 				arr_sum[i][j] -= second.arr[i][j];
-// 		}
-// 	}
-// 	return CData0(fname_sum, arr_sum, N_sum);
-// }
+CData0 operator-(const CPoly2 &first, const CPoly2 &second)
+{
+	int N_sum = max(first.N, second.N);
+	vector<vector<int>> arr_sum;
+	string fname_sum = first.cfname;
+	arr_sum.resize(N_sum);
+	for (int i = 0; i < N_sum; i++)
+	{
+		arr_sum[i].resize(N_sum);
+		for (int j = 0; j < N_sum; j++)
+		{
+			arr_sum[i][j] = 0;
+			if (i < first.N and j < first.N)
+				arr_sum[i][j] -= first.arr[i][j];
+			if (i < second.N and j < second.N)
+				arr_sum[i][j] -= second.arr[i][j];
+		}
+	}
+	CData0 sum(fname_sum, arr_sum, N_sum);
+	// cout << "end sum\n";
+	return sum;
+}
 
-// CData0 operator--(CPoly2 &th)
-// {
-// 	for (int i = 0; i < th.N; i++)
-// 	{
-// 		for (int j = 0; j < th.N - 1; j++)
-// 		{
-// 			th.arr[i][j] = (j + 1) * th.arr[i][j + 1];
-// 		}
-// 		th.arr[i][th.N - 1] = 0;
-// 	}
-// 	return th;
-// }
-// CData0 operator--(CPoly2 &th, int)
-// {
-// 	CData0 tmp(th);
-// 	for (int i = 0; i < th.N; i++)
-// 	{
-// 		for (int j = 0; j < th.N - 1; j++)
-// 		{
-// 			th.arr[i][j] = (j + 1) * th.arr[i][j + 1];
-// 		}
-// 		th.arr[i][th.N - 1] = 0;
-// 	}
-// 	return tmp;
-// }
-// CData0 operator++(CPoly2 &th)
-// {
-// 	for (int i = 0; i < th.N; i++)
-// 	{
-// 		for (int j = 0; j < th.N - 1; j++)
-// 		{
-// 			th.arr[j][i] = (j + 1) * th.arr[j + 1][i];
-// 		}
-// 		th.arr[th.N - 1][i] = 0;
-// 	}
-// 	return th;
-// }
-// CData0 operator++(CPoly2 &th, int)
-// {
-// 	CData0 tmp(th);
-// 	for (int i = 0; i < th.N; i++)
-// 	{
-// 		for (int j = 0; j < th.N - 1; j++)
-// 		{
-// 			th.arr[j][i] = (j + 1) * th.arr[j + 1][i];
-// 		}
-// 		th.arr[th.N - 1][i] = 0;
-// 	}
-// 	return tmp;
-// }
+CData0 operator--(CPoly2 &th)
+{
+	for (int i = 0; i < th.N; i++)
+	{
+		for (int j = 0; j < th.N - 1; j++)
+		{
+			th.arr[i][j] = (j + 1) * th.arr[i][j + 1];
+		}
+		th.arr[i][th.N - 1] = 0;
+	}
+	return th;
+}
+CData0 operator--(CPoly2 &th, int)
+{
+	CData0 tmp(th);
+	for (int i = 0; i < th.N; i++)
+	{
+		for (int j = 0; j < th.N - 1; j++)
+		{
+			th.arr[i][j] = (j + 1) * th.arr[i][j + 1];
+		}
+		th.arr[i][th.N - 1] = 0;
+	}
+	return tmp;
+}
+CData0 operator++(CPoly2 &th)
+{
+	for (int i = 0; i < th.N; i++)
+	{
+		for (int j = 0; j < th.N - 1; j++)
+		{
+			th.arr[j][i] = (j + 1) * th.arr[j + 1][i];
+		}
+		th.arr[th.N - 1][i] = 0;
+	}
+	return th;
+}
+CData0 operator++(CPoly2 &th, int)
+{
+	CData0 tmp(th);
+	for (int i = 0; i < th.N; i++)
+	{
+		for (int j = 0; j < th.N - 1; j++)
+		{
+			th.arr[j][i] = (j + 1) * th.arr[j + 1][i];
+		}
+		th.arr[th.N - 1][i] = 0;
+	}
+	return tmp;
+}
 
 int main()
 {
-
-	int len_parr = 0, size_parr = 4;
-	char str[128];
 	ofstream fout("output.txt", ios_base::trunc);
 	fout.close();
+	CFabricData *f[2] = {new CFabricData0(), new CFabricData1()};
+	char str[128];
+
 	ifstream fin("input.txt");
 
-	CFabricData *f[2] = {new CFabricData0(), new CFabricData1()};
-	CPoly2 **parr = new CPoly2 *[size_parr], **tmp_parr = nullptr;
+	vector<CPoly2 *> parr;
+	vector<char[128]> str_r;
+	str_r.reserve(4);
+	parr.reserve(4);
 
 	while (fin.getline(str, 127))
 	{
 
-		parr[len_parr] = CPoly2::CreateData(str, f);
-		if (parr[len_parr] != 0)
-			len_parr++;
-		if (len_parr == size_parr)
+		parr.push_back(CPoly2::CreateData(str, f));
+		if (!parr.back())
 		{
-			size_parr *= 2;
-			tmp_parr = new CPoly2 *[size_parr];
-			for (int i = 0; i < len_parr; i++)
-			{
-				tmp_parr[i] = parr[i];
-			}
-			delete[] parr;
-			parr = tmp_parr;
+			parr.pop_back();
+		}
+		if (parr.size() == parr.capacity())
+		{
+			parr.reserve(parr.size() * 2);
+		}
+	}
+	while (fin.getline(str, 127))
+	{
+
+		parr.push_back(CPoly2::CreateData(str, f));
+		if (!parr.back())
+		{
+			parr.pop_back();
+		}
+		if (parr.size() == parr.capacity())
+		{
+			parr.reserve(parr.size() * 2);
 		}
 	}
 	fin.close();
-	for (int i = 0; i < len_parr; i++)
 	{
-		parr[i]->output();
-	}
-	CPoly2 *tmp = new CData0(*parr[0]);
-	for (int i = 0; i < len_parr - 1; i++)
-	{
-		*parr[i] = *parr[i + 1];
-	}
-	*parr[len_parr - 1] = *tmp;
-	// CPoly2 *sum = new CData0();
-	// sum->output();
-	// for (int i = 0; i < len_parr; i++)
-	// {
-	// 	*sum = *parr[i] + *sum;
-	// 	sum->output();
-	// }
-	// sum->output();
-	delete tmp;
-	// delete sum;
 
-	// for (int i = 0; i < len_parr; i++)
-	// {
-	// 	(*parr[i])++;
-	// }
-	for (int i = 0; i < len_parr; i++)
-	{
-		parr[i]->output();
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			parr[i]->output();
+		}
+		CPoly2 *tmp = new CData0(*parr[0]);
+		for (unsigned int i = 0; i < parr.size() - 1; i++)
+		{
+			*parr[i] = *parr[i + 1];
+		}
+		*parr[parr.size() - 1] = *tmp;
+		CPoly2 *sum = new CData0();
+		sum->output();
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			*sum = *parr[i] + *sum;
+			sum->output();
+		}
+		sum->output();
+		delete tmp;
+		delete sum;
+
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			(*parr[i])++;
+		}
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			parr[i]->output();
+		}
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			(*parr[i])--;
+		}
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			parr[i]->output();
+		}
+		for (unsigned int i = 0; i < parr.size(); i++)
+		{
+			delete parr[i];
+			parr[i] = nullptr;
+		}
 	}
-	// for (int i = 0; i < len_parr; i++)
-	// {
-	// 	(*parr[i])--;
-	// }
-	// for (int i = 0; i < len_parr; i++)
-	// {
-	// 	parr[i]->output();
-	// }
-	for (int i = 0; i < len_parr; i++)
-	{
-		delete parr[i];
-		parr[i] = nullptr;
-	}
-	delete[] parr;
-	parr = nullptr;
-	for (int i = 0; i < 2; i++)
+	for (unsigned int i = 0; i < 2; i++)
 	{
 		delete f[i];
 		f[i] = nullptr;
 	}
-
 	return 0;
 }
 // вводится 1 y x xy
